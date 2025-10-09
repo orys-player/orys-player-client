f3cf625fc5306e6909bbebab74503f6d48607cb1
horameus <ludo.hartmann@gmail.com>
2025-10-09 18:55:13 +0200

    shadow and gradient modif

--- src/components/common/customButton/_sxs.ts
+++ src/components/common/customButton/_sxs.ts
@@ -2,8 +2,9 @@ export function useSxs(gradient?: boolean) {
     return {
         customButton: {
             background: gradient
-                ? 'linear-gradient(90deg,rgba(119, 58, 180, 1) 0%, rgba(29, 227, 253, 1) 50%, rgba(68, 255, 0, 1) 99%)'
+                ? 'linear-gradient(90deg,rgba(76, 18, 128, 1) 0%, rgba(20, 73, 117, 1) 50%, rgba(54, 163, 15, 1) 99%);'
                 : 'none',
+            textShadow: '#000000 0px 2px 3px',
         },
     };
 }
