8d0c0dbfe767d0a5adb98505175803b0fbeedff3
horameus <ludo.hartmann@gmail.com>
2025-10-13 21:54:48 +0200

    react-player and Youtube API config to handle video render and their metadata

--- src/components/common/customButton/_sxs.ts
+++ src/components/common/customButton/_sxs.ts
@@ -4,7 +4,7 @@ export function useSxs(gradient?: boolean) {
             background: gradient
                 ? 'linear-gradient(90deg,rgba(105, 0, 196, 1) 0%, rgba(0, 170, 212, 1) 50%, rgba(67, 250, 0, 1) 99%);'
                 : 'none',
-            textShadow: '#000000 0px 2px 3px',
+            textShadow: '#000000 0px 2px 2px',
         },
     };
 }
--- src/components/players/myYoutubePlayer/MyYoutubePlayer.tsx
+++ src/components/players/myYoutubePlayer/MyYoutubePlayer.tsx
@@ -1,16 +1,43 @@
 import { Stack, Typography } from '@mui/material';
 import React from 'react';
 import { useSxs } from './_sxs';
+import ReactPlayer from 'react-player';
 
 export function MyYoutubePlayer() {
-    const playerRef = React.useRef<HTMLDivElement>(null);
-
     const sxs = useSxs();
+
+    const [title, setTitle] = React.useState('');
+    const videoUrl = 'https://www.youtube.com/watch?v=FmHGsLskU5k';
+    const videoId = new URLSearchParams(new URL(videoUrl).search).get('v');
+
+    const apiKey = import.meta.env.VITE_YOUTUBE_API_KEY;
+    console.log('API Key:', apiKey);
+
+    React.useEffect(() => {
+        async function fetchVideoTitle() {
+            const apiUrl = `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${apiKey}`;
+
+            try {
+                const response = await fetch(apiUrl);
+                const data = await response.json();
+                if (data.items && data.items.length > 0) {
+                    setTitle(data.items[0].snippet.title);
+                } else {
+                    setTitle('Unknown Video');
+                }
+            } catch (error) {
+                console.error('Error fetching video title:', error);
+                setTitle('Error loading title');
+            }
+        }
+
+        fetchVideoTitle();
+    }, [videoId]);
 
     return (
         <Stack>
-            <Typography>My YouTube Player</Typography>
-            <div ref={playerRef} style={sxs.myYoutubePlayer} />
+            <Typography>{title}</Typography>
+            <ReactPlayer style={sxs.myYoutubePlayer} src={videoUrl} controls={true} />
         </Stack>
     );
 }
